cff4c80726226d9d9f9d2959f399b3952c0d6cdc
devgiordane <bruno_giordane@outlook.com>
2024-11-27 22:39:09 -0300

    Backup

--- site/components/Planos.vue
+++ site/components/Planos.vue
@@ -11,31 +11,50 @@
       </p>
       <div class="body">
         <div class="features">
-          <div class="feature"><p>Fotos para Feed</p></div>
-          <div class="feature"><p>Reels</p></div>
-          <div class="feature"><p>Versão Story</p></div>
-          <div class="feature"><p>Agendamento de Posts</p></div>
-          <div class="feature"><p>Elaboração do contéudo</p></div>
-          <div class="feature"><p>Suporte</p></div>
+          <div class="feature">
+            <p>Fotos para Feed</p>
+          </div>
+          <div class="feature">
+            <p>Reels</p>
+          </div>
+          <div class="feature">
+            <p>Versão Story</p>
+          </div>
+          <div class="feature">
+            <p>Agendamento de Posts</p>
+          </div>
+          <div class="feature">
+            <p>Elaboração do contéudo</p>
+          </div>
+          <div class="feature">
+            <p>Suporte</p>
+          </div>
         </div>
         <div class="values">
-          <div class="value"><p>12 por mês</p></div>
-          <div class="value"><p>04 por mês</p></div>
-          <div class="value"><p>04 por mês</p></div>
-          <div class="value"><p>Sim</p></div>
-          <div class="value"><p>Sim</p></div>
+          <div class="value">
+            <p>12 por mês</p>
+          </div>
+          <div class="value">
+            <p>04 por mês</p>
+          </div>
+          <div class="value">
+            <p>04 por mês</p>
+          </div>
+          <div class="value">
+            <p>Sim</p>
+          </div>
+          <div class="value">
+            <p>Sim</p>
+          </div>
           <div class="value">
             <p>Das 8h às 18h, Seg. a Sáb., via WhatsApp</p>
           </div>
         </div>
       </div>
       <div class="btnspace">
-        <a
-          target="_blank"
+        <a target="_blank"
           href="https://api.whatsapp.com/send/?phone=558893839112&text=Ol%C3%A1,%20vim%20do%20seu%20site,%20e%20quero%20saber%20mais%20sobre%20o%20plano%20*First%20Impression*!"
-          class="btn btn-dark"
-          >Quero saber mais</a
-        >
+          class="btn btn-dark">Quero saber mais</a>
       </div>
     </div>
     <div class="plano bg1">
@@ -48,32 +67,51 @@
       </p>
       <div class="body">
         <div class="features">
-          <div class="feature"><p>Posts no Feed</p></div>
-          <div class="feature"><p>Reels</p></div>
-          <div class="feature"><p>Versão Story</p></div>
+          <div class="feature">
+            <p>Posts no Feed</p>
+          </div>
+          <div class="feature">
+            <p>Reels</p>
+          </div>
+          <div class="feature">
+            <p>Versão Story</p>
+          </div>
 
-          <div class="feature"><p>Agendamento de Posts</p></div>
-          <div class="feature"><p>Elaboração do contéudo</p></div>
-          <div class="feature"><p>Suporte</p></div>
+          <div class="feature">
+            <p>Agendamento de Posts</p>
+          </div>
+          <div class="feature">
+            <p>Elaboração do contéudo</p>
+          </div>
+          <div class="feature">
+            <p>Suporte</p>
+          </div>
         </div>
         <div class="values">
-          <div class="value"><p>20 por mês</p></div>
-          <div class="value"><p>04 por mês</p></div>
-          <div class="value"><p>05 por mês</p></div>
-          <div class="value"><p>Sim</p></div>
-          <div class="value"><p>Sim</p></div>
+          <div class="value">
+            <p>20 por mês</p>
+          </div>
+          <div class="value">
+            <p>04 por mês</p>
+          </div>
+          <div class="value">
+            <p>05 por mês</p>
+          </div>
+          <div class="value">
+            <p>Sim</p>
+          </div>
+          <div class="value">
+            <p>Sim</p>
+          </div>
           <div class="value">
             <p>Das 8h às 18h, Seg. a Sáb., via WhatsApp</p>
           </div>
         </div>
       </div>
       <div class="btnspace">
-        <a
-          target="_blank"
+        <a target="_blank"
           href="https://api.whatsapp.com/send/?phone=558893839112&text=Ol%C3%A1,%20vim%20do%20seu%20site,%20e%20quero%20saber%20mais%20sobre%20o%20plano%20*Brand%20Booster*!"
-          class="btn btn-light"
-          >Quero saber mais</a
-        >
+          class="btn btn-light">Quero saber mais</a>
       </div>
     </div>
     <div class="plano bg2">
@@ -85,55 +123,49 @@
       </p>
       <div class="body">
         <div class="features">
-          <div class="feature"><p>Posts no Feed</p></div>
-          <div class="feature"><p>Reels</p></div>
-          <div class="feature"><p>Versão Story</p></div>
+          <div class="feature">
+            <p>Posts no Feed</p>
+          </div>
+          <div class="feature">
+            <p>Reels</p>
+          </div>
+          <div class="feature">
+            <p>Versão Story</p>
+          </div>
 
-          <div class="feature"><p>Agendamento de Posts</p></div>
-          <div class="feature"><p>Elaboração do contéudo</p></div>
-          <div class="feature"><p>Suporte</p></div>
+          <div class="feature">
+            <p>Agendamento de Posts</p>
+          </div>
+          <div class="feature">
+            <p>Elaboração do contéudo</p>
+          </div>
+          <div class="feature">
+            <p>Suporte</p>
+          </div>
         </div>
         <div class="values">
           <div class="value">
-            <button
-              @click="qtd.posts > 0 ? qtd.posts-- : qtd.posts"
-              class="btn-increase"
-            >
-              -</button
-            ><span class="field">{{ qtd.posts }}</span
-            ><button @click="qtd.posts++" class="btn-increase">+</button>
+            <button @click="qtd.posts > 0 ? qtd.posts-- : qtd.posts" class="btn-increase">
+              -</button><span class="field">{{ qtd.posts }}</span><button @click="qtd.posts++"
+              class="btn-increase">+</button>
           </div>
 
           <div class="value">
-            <button
-              @click="qtd.reels > 0 ? qtd.reels-- : qtd.reels"
-              class="btn-increase"
-            >
-              -</button
-            ><span class="field">{{ qtd.reels }}</span
-            ><button @click="qtd.reels++" class="btn-increase">+</button>
+            <button @click="qtd.reels > 0 ? qtd.reels-- : qtd.reels" class="btn-increase">
+              -</button><span class="field">{{ qtd.reels }}</span><button @click="qtd.reels++"
+              class="btn-increase">+</button>
           </div>
           <div class="value">
-            <button
-              @click="qtd.stories > 0 ? qtd.stories-- : qtd.stories"
-              class="btn-increase"
-            >
-              -</button
-            ><span class="field">{{ qtd.stories }}</span
-            ><button @click="qtd.stories++" class="btn-increase">+</button>
+            <button @click="qtd.stories > 0 ? qtd.stories-- : qtd.stories" class="btn-increase">
+              -</button><span class="field">{{ qtd.stories }}</span><button @click="qtd.stories++"
+              class="btn-increase">+</button>
           </div>
           <div class="value">
-            <input
-              type="checkbox"
-              v-model="qtd.agendamento"
-              id="agendamento"
-            /><label for="agendamento">agendamento</label>
+            <input type="checkbox" v-model="qtd.agendamento" id="agendamento" /><label
+              for="agendamento">agendamento</label>
           </div>
           <div class="value">
-            <input type="checkbox" v-model="qtd.conteudo" id="conteudo" /><label
-              for="conteudo"
-              >conteudo</label
-            >
+            <input type="checkbox" v-model="qtd.conteudo" id="conteudo" /><label for="conteudo">conteudo</label>
           </div>
 
           <div class="value">
@@ -142,12 +174,9 @@
         </div>
       </div>
       <div class="btnspace">
-        <a
-          target="_blank"
+        <a target="_blank"
           :href="`https://api.whatsapp.com/send/?phone=558893839112&text=Ol%C3%A1,%20vim%20do%20seu%20site,%20e%20quero%20saber%20mais%20sobre%20o%20plano%20*Made%20on%20demand*!\n\n\ ${msg}`"
-          class="btn btn-light"
-          >Quero saber mais</a
-        >
+          class="btn btn-light">Quero saber mais</a>
       </div>
     </div>
   </div>
@@ -171,22 +200,22 @@ export default {
       let preco = 0;
       let qtd = this.qtd;
       if (qtd.posts) {
-        preco += qtd.posts * 10;
+        preco += qtd.posts * 12;
       }
 
       if (qtd.stories) {
         preco += qtd.stories * 3;
       }
       if (qtd.reels) {
-        preco += qtd.reels * 15;
+        preco += qtd.reels * 25;
       }
 
       if (qtd.agendamento) {
-        preco += 30;
+        preco += 50;
       }
 
       if (qtd.conteudo) {
-        preco += (qtd.posts + qtd.reels) * 3;
+        preco += (qtd.posts + qtd.reels) * 4;
       }
       return preco;
     },
@@ -220,12 +249,15 @@ export default {
 <style lang="scss">
 .planos {
   background: white;
+
   .plan-title {
     font-size: 25px;
     text-align: center;
     padding: 2rem 0;
   }
+
   padding: 10px;
+
   .plano {
     border-radius: 10px;
     margin-bottom: 3rem;
@@ -233,31 +265,38 @@ export default {
     display: flex;
     flex-direction: column;
     align-items: center;
+
     h2 {
       font-size: 35px;
       text-align: center;
     }
+
     &.bg1 {
       background: #dbeafe;
     }
+
     &.bg2 {
       background: #f3f4f6;
     }
+
     h3 {
       font-size: 30px;
       text-align: center;
       padding: 10px 0;
+
       span {
         font-weight: 400;
         font-size: 15px;
       }
     }
+
     .subtitle {
       text-align: center;
     }
 
     .body {
       display: flex;
+
       .features {
         .feature {
           display: flex;
@@ -265,6 +304,7 @@ export default {
           justify-content: flex-end;
           min-height: 40px;
           padding-right: 10px;
+
           p {
             text-align: end;
             font-weight: 700;
@@ -272,6 +312,7 @@ export default {
           }
         }
       }
+
       .values {
         .value {
           display: flex;
@@ -287,17 +328,20 @@ export default {
             font-weight: 700;
             font-size: 20px;
           }
+
           .field {
             padding: 0 10px;
           }
         }
       }
     }
+
     .btnspace {
       padding: 20px 0;
       text-align: center;
     }
   }
+
   @media only screen and (min-width: 1200px) {
     padding: 5rem 10rem;
   }
